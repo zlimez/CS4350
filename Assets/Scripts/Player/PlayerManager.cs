2ad42d5007f5aea2a36df9f069b96ffdfdac1654
CooolKey <986281362@qq.com>
2024-10-02 05:33:48 +0800

    main panel

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -7,7 +7,7 @@ namespace Abyss.Player
         [SerializeField] PlayerController playerController;
         [SerializeField] Weapon weapon;
         [SerializeField] PlayerAttr playerAttributes;
-        public float health;
+        // public float health;
         public System.Action OnDeath;
 
         void OnEnable()
@@ -32,8 +32,8 @@ namespace Abyss.Player
         public bool TakeHit(float baseDamage)
         {
             if (playerController.TakeHit()) return false; // Is still taking last damage
-            health -= baseDamage;
-            bool isDead = health <= 0;
+            playerAttributes.health -= baseDamage;
+            bool isDead = playerAttributes.health <= 0;
             if (isDead)
                 OnDeath?.Invoke();
             return isDead;
